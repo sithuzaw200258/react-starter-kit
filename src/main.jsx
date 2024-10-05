b3d6eed75c79740b7d096e2754197f66073f00bb
unknown <sithuzaw200258@gmail.com>
2024-10-05 15:24:17 +0630

    Initial State

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import 'flowbite';
+
+createRoot(document.getElementById('root')).render(
+  <App />
+)
